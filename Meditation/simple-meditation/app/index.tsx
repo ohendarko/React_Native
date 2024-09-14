cf8b5d3d58bc563afce99facd8bf271eb45ba584
ohendarko <ohendarko@gmail.com>
2024-09-14 18:44:40 +0000

    import issues resolved. Added backgtround and titles

--- Meditation/simple-meditation/app/index.tsx
+++ Meditation/simple-meditation/app/index.tsx
@@ -1,18 +1,84 @@
+import { View, Text, ImageBackground, StyleSheet } from 'react-native';
 import React from 'react';
-import { Image, StyleSheet, Platform, View, Text }from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { StatusBar } from 'expo-status-bar';
+import CustomButton from '../components/CustomButton';
 
-export default function HomeScreen () {
+
+const beachImage = require('../assets/meditation-images/beach.png');
+
+const App = () => {
   return (
-    <View style={styles.container}>
-    <Text>Hello World</Text>
+    <View style={{ flex: 1 }}>
+      <ImageBackground source={beachImage}
+      style={{ flex: 1 }}
+      resizeMode='cover'>
+        <LinearGradient colors={['rgba(0,0,0,0.4)', 'rgba(0, 0, 0, 0.8)']}
+          style={styles.gradient}>
+          <SafeAreaView style={styles.safeAreaView}>
+            <View style={styles.textContainer}>
+              <Text style={styles.safeareaviewtext}>
+               Simple Meditation
+              </Text>
+              <Text style={styles.subtext}>
+                Simplifying Meditation for Everyone
+              </Text>
+            </View>
+
+            <View>
+              <CustomButton onPress={() => console.log('tap')} title='Get Started' />
+            </View>
+
+            <StatusBar style="light" />
+          </SafeAreaView>          
+        </LinearGradient>        
+      </ImageBackground>
+      
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: 'center',
     flex: 1,
-    justifyContent: 'center',
+  },
+  imageBackground: {
+    flex: 1,
+  },
+  gradient: {
+    flex: 1,
+    // justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: 'white',
+    fontSize: 40,
+    fontWeight: 'bold',
+  },
+  safeAreaView: {
+    flex: 1,
+    //justifyContent: 'center',
+  },
+  safeareaviewtext: {
+    color: 'white',
+    fontSize: 40,
+    fontWeight: 'bold',
+    paddingVertical: 7,
+    justifyContent: 'space-between',
+    marginTop: 10
+  },
+  textContainer: {
+    alignItems: 'center',
+  },
+  subtext: {
+    textAlign: 'center',
+    color: 'white',
+    fontSize: 25,
+    marginTop: 3,
+    flex: 0.9
   },
 });
+
+
+export default App;
---
+++ Meditation/simple-meditation/components/CustomButton.tsx
@@ -0,0 +1,39 @@
+import { View, Text, TouchableOpacity, StyleSheet, TextStyle } from 'react-native';
+import React from 'react';
+
+interface CustomButtonProps {
+  onPress: () => void;
+  title: string;
+  textStyles?: TextStyle;
+  containerStyles?: TextStyle;
+}
+
+const CustomButton = ({ onPress, title, textStyles= {}, containerStyles= {} }: CustomButtonProps) => {
+  return (
+    <TouchableOpacity activeOpacity={0.7}
+      style={[styles.touchableOpacity, containerStyles]}
+      onPress={onPress}>
+      <Text style={[styles.text, textStyles]}>{title}</Text>
+    </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+  touchableOpacity: {
+    backgroundColor: 'white',
+    borderRadius: 20,
+    minHeight: 62,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  text: {
+    color: 'black',
+    fontSize: 20,
+    fontWeight: '600',
+  },
+  
+})
+
+
+export default CustomButton;
+
