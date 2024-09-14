884b537e3f680bf296c893e03981b1b7908d5e4a
ohendarko <ohendarko@gmail.com>
2024-09-14 23:27:42 +0000

    increased bottom padding of button

--- Meditation/simple-meditation/components/CustomButton.tsx
+++ Meditation/simple-meditation/components/CustomButton.tsx
@@ -26,7 +26,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     marginHorizontal: 10,
-    marginBottom: 15
+    marginBottom: 75
   },
   text: {
     color: 'black',
