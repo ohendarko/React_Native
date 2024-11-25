b430f923cd1be1ac136fff52e27ef5f810c88c1e
ohendarko <ohendarko@gmail.com>
2024-11-25 13:14:53 +0000

    Added meditation timer

--- Meditation/simple-meditation/app/_layout.tsx
+++ Meditation/simple-meditation/app/_layout.tsx
@@ -1,6 +1,23 @@
-import { Slot, Stack } from 'expo-router';
+import { Slot, SplashScreen, Stack } from 'expo-router';
+import { useFonts } from 'expo-font';
+import { useEffect } from 'react';
+
+//This will prevent the splash screen from auto hiding until loading all the font assets.
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout () {
+  const [fontsLoaded, error] = useFonts({
+    "Roboto-Mono": require("../assets/fonts/RobotoMono-Regular.ttf")
+  });
+
+  useEffect(() => {
+    if (error) throw error;
+    if (fontsLoaded) SplashScreen.hideAsync();
+  }, [fontsLoaded, error]);
+
+  if (!fontsLoaded) return null;
+  if (!fontsLoaded && !error) return null;
+
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
--- Meditation/simple-meditation/app/meditate/[id].tsx
+++ Meditation/simple-meditation/app/meditate/[id].tsx
@@ -1,12 +1,44 @@
 import { View, Text, StyleSheet, ImageBackground, Pressable } from 'react-native';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import MEDITATION_IMAGES from '@/constants/meditation-images';
 import AppGradient from '@/components/AppGradient';
 import { router, useLocalSearchParams } from 'expo-router';
 import AntDesign from '@expo/vector-icons/AntDesign';
+import CustomButton from '@/components/CustomButton';
 
 const Meditate = () => {
   const { id }   =  useLocalSearchParams();
+
+  const [secondsRemaining, setSecondsRemaining] = useState(10);
+
+  const [isMeditating, setMeditating] = useState(false);
+
+  useEffect(() => {
+      let timerId: NodeJS.Timeout;
+
+      //Exit
+      if (secondsRemaining === 0) {
+        setMeditating(false);
+        return;
+      }
+
+      if (isMeditating) {
+        timerId = setTimeout(() => {
+            setSecondsRemaining(secondsRemaining - 1);
+        }, 1000)
+      }
+
+
+      return () => {
+        clearTimeout(timerId);
+      }
+  }, [secondsRemaining, isMeditating]);
+
+  //Format the time left to ensure two digits are displayed
+  const formattedTimeMinutes = String(Math.floor(secondsRemaining / 60)).padStart(2, "0")
+
+  const formattedTimeSeconds = String(Math.floor(secondsRemaining % 60)).padStart(2, "0")
+
 
   return (
     <View style={styles.view}>
@@ -18,6 +50,21 @@ const Meditate = () => {
           <AntDesign name="leftcircleo" size={40} color="white" />
           </Pressable>
           
+          <View style={styles.viewTwo}>
+            <View style={styles.viewText}>
+              <Text style={styles.text}>
+                {formattedTimeMinutes}:{formattedTimeSeconds}
+              </Text>
+            </View>
+          </View>
+
+          <View style={styles.viewThree}>
+            <CustomButton
+              title="Start Meditatiion"
+              onPress={() => setMeditating(true)}
+            />
+          </View>
+
         </AppGradient>
         
       </ImageBackground>
@@ -40,6 +87,29 @@ const styles = StyleSheet.create({
     left: 20,
     zIndex: 200
   },
+  viewTwo: {
+    display: "flex",
+    flex: 1,
+    justifyContent: "center",
+  },
+  viewText:  {
+    marginHorizontal: "auto",
+    borderRadius: 1000,
+    width: 180,
+    height: 180,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
+
+  },
+  text: {
+    fontSize: 35,
+    color: "indigo",
+    fontFamily: "Roboto-Mono",
+  },
+  viewThree: {
+    marginBottom: 5
+  }
 })
 
 
