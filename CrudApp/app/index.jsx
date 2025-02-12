839a3d171664f3103e83ca7ce993e02eef5bfef6
ohendarko <ohendarko@gmail.com>
2025-02-12 22:27:08 +0000

    CRUD app completed

--- CrudApp/app/index.jsx
+++ CrudApp/app/index.jsx
@@ -1,7 +1,9 @@
-import { Text, View, TextInput, Pressable, StyleSheet } from "react-native";
+import { Text, View, TextInput, Pressable, StyleSheet, FlatList } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useState } from "react";
 import { data } from "@/data/todos";
+
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 
 export default function Index() {
   const [todos, setTodos] = useState(data.sort((a,b) => b.id - a.id));
@@ -26,20 +28,44 @@ export default function Index() {
     setTodos(todos.filter(todo => todo.id !== id))
   }
 
+  const renderItem = ({ item }) => (
+    <View style={styles.todoItem}>
+      <Text
+        style={[styles.todoText, item.completed && styles.completedText]}
+        onPress={() => toggleTodo(item.id)}
+      >
+        {item.title}
+      </Text>
+      <Pressable
+        onPress={() => removeTodo(item.id)}
+      >
+        <MaterialCommunityIcons name="delete-circle" size={36} color="red" selectable={undefined} />
+      </Pressable>
+    </View>
+  )
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.inputContainer}>
         <TextInput
+          type='text'
           style={styles.input}
           placeholder="Add a new todo"
           placeholderTextColor="gray"
           value={text}
-          onChange={setText}
+          onChangeText={setText}
         />
         <Pressable onPress={addTodo} style={styles.addButton}>
-          <Text style={styles.AddButtonText}>Add</Text>
+          <Text style={styles.addButtonText}>Add</Text>
         </Pressable>
       </View>
+
+      <FlatList
+        data={todos}
+        renderItem={renderItem}
+        keyExtractor={todo => todo.id}
+        contentContainerStyle={{ flexGrow: 1 }}
+      />
       
     </SafeAreaView>
   );
@@ -58,6 +84,48 @@ const styles = StyleSheet.create({
     width: '100%',
     maxwidth: 1024,
     marginHorizontal: 'auto',
-    marginHorizontal
+    pointerEvents: 'auto',
+  },
+  input: {
+    flex: 1,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 10,
+    marginRight: 10,
+    fontSize: 18,
+    minWidth: 0,
+    color: 'white'
+  },
+  addButton: {
+    backgroundColor: 'white',
+    borderRadius: 5,
+    padding: 10,
+  },
+  addButtonText: {
+    fontSize: 18,
+    color: 'black'
+  },
+  todoItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    gap: 4,
+    padding: 10,
+    borderBottomColor: 'gray',
+    borderBottomWidth: 1,
+    width: '100%',
+    maxWidth: 1024,
+    marginHorizontal: 'auto',
+    pointerEvents: 'auto',
+  },
+  todoText: {
+    flex: 1,
+    fontSize: 18,
+    color: 'white',
+  },
+  completedText: {
+    textDecorationLine: 'line-through',
+    color: 'gray',
   }
 })
