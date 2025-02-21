f1751beadaec74653fc10181bff18f622d63f159
ohendarko <ohendarko@gmail.com>
2025-02-21 00:36:54 +0000

    added tabs section for Aora. Im getting a hang of the tailwind

--- Aora/app/index.jsx
+++ Aora/app/index.jsx
@@ -1,1 +1,15 @@
-import "./global.css";
+import { View, Text } from 'react-native';
+import React from 'react';
+import "@/global.css";
+import { Link } from 'expo-router';
+
+const index = () => {
+  return (
+    <View  className="flex-1 items-center justify-center bg-white">
+      <Text  className="text-3xl font-pblack">Aora!</Text>
+      <Link href="/home" style={{ color: 'blue' }}>Go to Home</Link>
+    </View>
+  )
+}
+
+export default index
