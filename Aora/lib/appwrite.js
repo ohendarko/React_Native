59b94a1c4d1a9611392d4db62172de7e10e85c86
ohendarko <ohendarko@gmail.com>
2025-02-23 17:32:20 +0000

    working on appwrite logic. This feels completely new

--- Aora/lib/appwrite.js
+++ Aora/lib/appwrite.js
@@ -4,5 +4,6 @@ export const appwriteConfig = {
   projectId: "67bb56900003f125727b",
   databaseId: "67bb58b60014ed62ce3f",
   userCollectionId: "67bb58eb00115e4e596e",
-  videoCollectionId:
+  videoCollectionId: "67bb5932001fd5c6dcf8",
+  storageId: "67bb5b6c00124cc7bffc"
 }
