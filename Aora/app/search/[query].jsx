c400f95d28588f94c2ef3e339517a6d73d148bc0
ohendarko <ohendarko@gmail.com>
2025-03-10 23:41:26 +0000

    pushing any unregistered changes?

--- Aora/app/search/[query].jsx
+++ Aora/app/search/[query].jsx
@@ -1,14 +1,15 @@
 import { View, Text } from 'react-native';
 import React from 'react';
 import { useLocalSearchParams } from 'expo-router';
+import { SafeAreaView } from 'react-native';
 
 const Search = () => {
   const { query } = useLocalSearchParams();
-  
+
   return (
-    <View>
-      <Text className="text-3xl">{query}</Text>
-    </View>
+    <SafeAreaView  className="bg-primary h-full">
+      <Text className="text-3xl text-white">{query}</Text>
+    </SafeAreaView>
   )
 }
 
--- Aora/components/SearchInput.jsx
+++ Aora/components/SearchInput.jsx
@@ -1,11 +1,11 @@
-import { View, Text, TextInput, TouchableOpacity, Image } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, Image, Alert } from 'react-native';
 import React from 'react';
 import { useState } from 'react';
 import { icons } from "@/constants";
-import { usePathname } from 'expo-router';
+import { router, usePathname } from 'expo-router';
 
 
-const SearchInput = ({ title, value, placeholder, handleChangeText, otherStyles, ...props }) => {
+const SearchInput = () => {
 
   const pathname =  usePathname();
   const [query, setQuery] = useState('');
@@ -14,14 +14,22 @@ const SearchInput = ({ title, value, placeholder, handleChangeText, otherStyles,
     <View className="border-2 border-black-200 w-full h-16 px-4 bg-black-100 rounded-2xl flex-row focus:border-secondary items-center sapce-x-4">
       <TextInput
         className="text-base mt-0.5 text-white flex-1 font-pregular"
-        value={value}
+        value={query}
         placeholder="Search for video topic"
-        placeholderTextColor="#7b7b8b"
-        onChangeText={handleChangeText}
-        secureTextEntry={title === 'Password' && !showPassword}
+        placeholderTextColor="#CDCDE0"
+        onChangeText={(e)  => setQuery(e)}
       />
       
-      <TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          if(!query) {
+            return Alert.alert('Missing Query', "Please input something to search results across database")
+          }
+
+          if(pathname.startsWith('/search')) router.setParams({ query })
+          else router.push(`/search/${query}`)
+        }}
+      >
         <Image
           source={icons.search}
           className="w-5 h-5"
